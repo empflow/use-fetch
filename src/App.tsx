f57430de8ad05eb10421b6db24ba2014c44c4220
Matvey <empflow@tutanota.com>
2023-08-16 19:33:38 +0300

    feat: add basic email & password form & use useFetch hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
-import React, { useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import useFetch from "./useFetch";
+import storeAuthRespData from "./utils/storeAuthRespData";
 
 function App() {
+  const [email, setEmail] = useState("Hayley.Schulist@yahoo.com");
+  const [pwd, setPwd] = useState("GffIiAUR_rv7urd0#");
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const { data, err, loading, setLoading, fetch } = useFetch({
+    url: "auth/sign-in",
+    method: "post",
+    body: {
+      email,
+      password: pwd,
+      captchaBypassToken: import.meta.env.VITE_CAPTCHA_BYPASS_TOKEN,
+    },
+    opts: { fetchImmediately: false, withAuth: false },
+  });
+
+  useEffect(() => {
+    if (!hasSubmitted) return;
+    storeAuthRespData(data);
+  }, [data]);
+
+  async function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+    setHasSubmitted(true);
+    await fetch();
+  }
+
   return (
     <div>
-      <h1>Home page</h1>
+      <form onSubmit={handleFormSubmit}>
+        email
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        password
+        <input
+          type="password"
+          value={pwd}
+          onChange={(e) => setPwd(e.target.value)}
+        />
+        <br />
+        <button type="submit">{loading ? "Loading..." : "Submit"}</button>
+      </form>
     </div>
   );
 }
