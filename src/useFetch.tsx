1499cd4076157d6b297712faba310bcc28b2d4ad
Matvey <empflow@tutanota.com>
2023-08-16 19:33:17 +0300

    wip: add useFetch (not fully implmeneted)

--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -1,3 +1,108 @@
-interface IUseFetchProps {}
+import { AxiosError, isAxiosError } from "axios";
+import axios from "./utils/axios";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-export default function useFetch() {}
+type HttpMethod =
+  | "get"
+  | "post"
+  | "put"
+  | "patch"
+  | "delete"
+  | "head"
+  | "options";
+
+interface Params {
+  url: string;
+  method: HttpMethod;
+  body?: any;
+  opts?: {
+    fetchImmediately?: boolean;
+    persistDataWhileFetching?: boolean;
+    withAuth?: boolean;
+  };
+}
+
+export default function useFetch<T extends unknown>({
+  url,
+  method,
+  body,
+  opts,
+}: Params) {
+  const {
+    fetchImmediately = true,
+    persistDataWhileFetching = true,
+    withAuth = true,
+  } = opts ?? {};
+
+  const [err, setErr] = useState<AxiosError | null>(null);
+  const [data, setData] = useState<null | T>(null);
+  const [loading, setLoading] = useState(false);
+  const notSignedInMsg = (
+    <>
+      <span>You are not signed in.</span> <a href="/sign-in">Sign in here</a>
+    </>
+  );
+
+  useEffect(() => {
+    if (fetchImmediately) fetch();
+  }, []);
+
+  async function fetch(customBody?: unknown) {
+    fetchSetup();
+
+    if (withAuth) fetchWithAuth(customBody);
+    else fetchWithoutAuth(customBody);
+  }
+
+  async function fetchWithoutAuth(customBody?: unknown) {
+    try {
+      const resp = await axios({ url, method, data: customBody ?? body });
+      setData(resp.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function fetchWithAuth(customBody?: unknown) {
+    try {
+      const accessToken = getAccessToken();
+      if (!accessToken) return;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function refreshTokens() {
+    const refreshToken = Cookies.get("refreshToken");
+    if (!refreshToken) return toast(notSignedInMsg);
+
+    try {
+      const refreshTokenDeserialized = JSON.parse(refreshToken);
+      const resp = await axios({
+        url: "http://localhost:3001/auth/get-new-tokens",
+        method,
+        data: { refreshToken },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  function fetchSetup() {
+    if (!persistDataWhileFetching) setData(null);
+    setErr(null);
+    setLoading(true);
+  }
+
+  function getAccessToken() {
+    return Cookies.get("accessToken");
+  }
+
+  function getAuthHeader(accessToken: string) {
+    return `Bearer ${accessToken}`;
+  }
+
+  return { err, data, loading, setLoading, fetch };
+}
