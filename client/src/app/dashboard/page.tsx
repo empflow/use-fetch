1e0d2e0b608543426d6832b8b97a2dcdca219451
Matvey <empflow@tutanota.com>
2023-08-17 15:18:35 +0300

    feat: add <NotSignedInPopup /> & remove test counter from /dashboard page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -3,11 +3,11 @@ import useAppDispatch from "@/hooks/useAppDispatch";
 import useAppSelector from "@/hooks/useAppSelector";
 import useFetch from "@/hooks/useFetch";
 import {
-  changeByAmount,
-  decrement,
-  increment,
-  setChangeByAmount,
-} from "@/redux/slices/counter";
+  hideNotSignedInPopup,
+  showNotSignedInPopup,
+  toggleNotSignedInPopupOpen,
+} from "@/redux/slices/auth";
+import NotSignedInPopup from "../components/NotSignedInPopup";
 
 export default function Dashboard() {
   const {
@@ -15,35 +15,26 @@ export default function Dashboard() {
     err: notesErr,
     fetch: notesFetch,
     loading: notesLoading,
-  } = useFetch({ url: "/notes", method: "get" });
+  } = useFetch({
+    url: "/notes",
+    method: "get",
+    opts: { withAuth: false, fetchImmediately: false },
+  });
   const {
     data: tagsData,
     err: tagsErr,
     fetch: tagsFetch,
     loading: tagsLoading,
-  } = useFetch({ url: "/tags", method: "get" });
-  const counter = useAppSelector((state) => state.counter);
-  const dispatch = useAppDispatch();
+  } = useFetch({
+    url: "/tags",
+    method: "get",
+    opts: { withAuth: false, fetchImmediately: false },
+  });
 
   return (
     <>
+      <NotSignedInPopup />
       <h1>dashboard</h1>
-
-      <div className="mt-10 mb-10">
-        <h2>Count: {counter.value}</h2>
-        <div className="flex gap-4">
-          <button onClick={() => dispatch(increment())}>Increment</button>
-          <button onClick={() => dispatch(decrement())}>Decrement</button>
-          <input
-            type="number"
-            value={counter.changeByAmount}
-            onChange={(e) => dispatch(setChangeByAmount(e.target.value))}
-          />
-          <button onClick={() => dispatch(changeByAmount())}>
-            Change count by {counter.changeByAmount}
-          </button>
-        </div>
-      </div>
 
       <div className="flex flex-col gap-5">
         <div>
@@ -60,7 +51,7 @@ export default function Dashboard() {
           <div>data: {JSON.stringify(tagsData)}</div>
           <div>error: {JSON.stringify(tagsErr)}</div>
           <div>
-            <button onClick={notesFetch}>fetch</button>
+            <button onClick={tagsFetch}>fetch</button>
           </div>
         </div>
       </div>
